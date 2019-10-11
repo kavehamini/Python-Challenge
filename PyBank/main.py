2be1f52e997fb7f24e39e055f63d1a0ea2f72bdc
Kaveh <kavehamini@live.com>
2019-10-11 18:34:02 -0400

    Update main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,16 +1,24 @@
 import os, csv
 Months=[] 
-uniquelist=[] 
+uniquelist=[]
+Profit_Losses=[]
 csvpath = os.path.join("Resources", "budget_data.csv")
 with open (csvpath, newline="") as csvfile:
     csvreader=csv.reader(csvfile, delimiter=",")
     next(csvreader)
     for row in csvreader:
-        Months.append(row[0])   
+        Months.append(row[0])
+        Profit_Losses.append(row[1])
+
     for x in Months:
         if x not in uniquelist:
             uniquelist.append(x)
-print(f'The total number of months in the database is {len(uniquelist)}')
+print(f'Total Months:{len(uniquelist)}')
+sum=0
+for num in Profit_Losses:
+    sum = sum + int(num)
+print (f'Total:${str(sum)} ')
+
 
 
 
