5aa354d88c2548e6b41bc6b33404d195bd99837e
Kaveh <kavehamini@live.com>
2019-10-15 10:53:10 -0400

    commiting changes

--- PyBank/main.py
+++ PyBank/main.py
@@ -18,7 +18,7 @@ print(f'Total Months:{len(uniquelist)}')
 sum=0
 for num in Profit_Losses:
     sum = sum + int(num)
-print (f'Total:${str(sum)} ')
+print (f'Total:${str(sum)}')
 
 diff = [int(Profit_Losses[i + 1]) - int(Profit_Losses[i]) for i in range(len(Profit_Losses)-1)]
 s = 0
@@ -31,8 +31,9 @@ print(f'Greatest Increase:{Months[maxindex]}, ${max(diff)}')
 minindex = diff.index(min(diff)) + 1
 print(f'Greatest Decrease:{Months[minindex]}, ${min(diff)}')
 
-#for i in range(len(Months)):
-    #mapped = zip(Months[i], diff[i]) 
-#print(mapped)
-#print(f'Greatest Increase:${max(diff)}')
-#print(f'Greatest Increase:${min(diff)}')
+finalresults = [ "Total Months: " + str(len(uniquelist)), " Total: $" + str(sum)," The average change: $" + str(s/len(diff)),
+                " Greatest Increase: " + str(Months[maxindex]) + ", $" + str(max(diff)), 
+                " Greatest Decrease: " + str(Months[minindex]) + ", $" + str(min(diff))]
+with open('Pybankresults.txt', 'w') as f:
+    for item in finalresults:
+        f.write(item)
