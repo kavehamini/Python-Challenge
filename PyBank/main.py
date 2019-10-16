0587f8abc745b0972450c72f29cf9849af190455
Kaveh <kavehamini@live.com>
2019-10-15 22:00:39 -0400

    committing the updates

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,19 +1,26 @@
+#importing os and csv modules
 import os, csv
-
-Months=[] 
+#creating Months list to convert the Months and years column of csv to a list
+Months=[]
+#creating uniquelist to get the unique values in the Months and Years list created 
 uniquelist=[]
+#creating profit_loss list to convert the profit_loss column of csv to a list
 Profit_Losses=[]
+#creating csvpath to locate the csv file and read it
 csvpath = os.path.join("Resources", "budget_data.csv")
 with open (csvpath, newline="") as csvfile:
     csvreader=csv.reader(csvfile, delimiter=",")
+    #skipping the header
     next(csvreader)
+    #converting the Months and Profil_loss columns of csv to lists
     for row in csvreader:
         Months.append(row[0])
         Profit_Losses.append(row[1])
-
+    #getting unique values for the Months and years to avoid repetition
     for x in Months:
         if x not in uniquelist:
             uniquelist.append(x)
+#printing the results
 print("Financial Analysis")
 print("..........................")
 print(f'Total Months:{len(uniquelist)}')
@@ -22,18 +29,20 @@ for num in Profit_Losses:
     sum = sum + int(num)
 print (f'Total:${str(sum)}')
 
+#geting the average difference and priting it after rounding it up to two decimal places
 diff = [int(Profit_Losses[i + 1]) - int(Profit_Losses[i]) for i in range(len(Profit_Losses)-1)]
 s = 0
 for n in diff:
     s = s + n
 print(f'The average change:${round(s/len(diff),2)}')
 
+#getting the indecies for max and min values in the list of differences and printing the max and min differences
 maxindex = diff.index(max(diff)) + 1
 print(f'Greatest Increase:{Months[maxindex]}, ${max(diff)}')
 minindex = diff.index(min(diff)) + 1
 print(f'Greatest Decrease:{Months[minindex]}, ${min(diff)}')
-
-finalresults = [ "Total Months: " + str(len(uniquelist)), " Total: $" + str(sum)," The average change: $" + str(s/len(diff)),
+#exporting the results as a text file
+finalresults = [ "Total Months: " + str(len(uniquelist)), " Total: $" + str(sum)," The average change: $" + str(round(s/len(diff),2)),
                 " Greatest Increase: " + str(Months[maxindex]) + ", $" + str(max(diff)), 
                 " Greatest Decrease: " + str(Months[minindex]) + ", $" + str(min(diff))]
 with open('Pybankresults.txt', 'w') as f:
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,42 +1,49 @@
+#importing os and csv modules
 import os, csv
-
+#creating votecount list to convert the votes column of csv to a list and count the votes cast
 votecount = []
-uniquelist= []
+#creating candidates list to get a list of all candidates from the candidates csv column 
 candidates = []
+#creating uniquelist to get a list of unique candidate names to avoid repetition
+uniquelist= []
 
+#creating csvpath to locate the csv file and read it
 csvpath = os.path.join("Resources", "election_data.csv")
 with open(csvpath, newline="") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
+    #skipping the header
     next(csvreader)
-
+    #converting the csv column of candidates to a list
     for row in csvreader:
         
         candidates.append(row[2])
+    #getting the unique candidate names
     for x in candidates:
         if x not in uniquelist:
             uniquelist.append(x)
     
-    
+#getting the vote count for each candidate    
 khancount = candidates.count("Khan")
 correycount = candidates.count("Correy")
 licount = candidates.count("Li")
 otooleycount = candidates.count("O'Tooley")
-
+#getting the total votes cast
 totalvotecount= khancount + correycount + licount + otooleycount
 
 
-
+#calculating the percentage of votes for each candidate and rounding it to three decimal places
 
 khanpercent = round((khancount/totalvotecount)*100,3)
 correypercent = round((correycount/totalvotecount)*100,3)
 lipercent = round((licount/totalvotecount)*100,3)
 otooleypercent = round((otooleycount/totalvotecount)*100,3)
 
+#printing the values obtained
 print("Election Results")
 print(".........................")
 print(f'Total votes:{totalvotecount}')
 print(".........................")
-print("A list of the canndidates who received votes:")
+print("The list of the candidates who received votes:")
 print(" ")
 for item in uniquelist:
     print(item)
@@ -46,7 +53,7 @@ print(f'Correy: {correypercent}%, {(correycount)}')
 print(f'Li: {lipercent}%, {licount}')
 print(f'O\'Tooley: {otooleypercent}%, {otooleycount}')
 print(".........................")
-
+# getting the winner by popular vote and printing it
 if khanpercent > correypercent and khanpercent > lipercent and khanpercent > otooleypercent:
     winner = "Khan"
 elif correypercent > khanpercent and correypercent > lipercent and correypercent > otooleypercent:
@@ -60,7 +67,7 @@ else:
 print(winner +" is the winner!" )
 
 
-
+#exporting the results as a text file
 finalresults = [ "Total votes: " + str(totalvotecount), ", Khan: " + str(khanpercent)+"%" + " ("+str(khancount)+")",
                 ", Correy: " + str(correypercent)+"%" + " ("+str(correycount)+")",
                 ", Li: " + str(lipercent) +"%" + " ("+str(licount)+")", 
